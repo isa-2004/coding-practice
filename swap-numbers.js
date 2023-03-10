419358865010bc542ec116193cee5969be0f704c
Isa <isa.aeraf@gmail.com>
2023-03-10 17:17:54 +0400

    updated js swap-numbers

--- swap-numbers.js
+++ swap-numbers.js
@@ -1,11 +1,11 @@
-let a = prompt('Enter first value');
-let b = prompt('Enter second value');
+let a = prompt('Enter first value: ');
+let b = prompt('Enter second value: ');
 
 let temp;
 
 temp = a;
 a = b;
-b = a;
+b = temp;
 
 console.log('after swapping a = '+ a);
 console.log('after swapping b = '+ b);
