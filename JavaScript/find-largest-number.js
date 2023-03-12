30b256f5985e836f73178ab550d1f6fec3c2e75f
Isa <isa.aeraf@gmail.com>
2023-03-12 16:27:24 +0400

    added a new js file find-largest-number.js

---
+++ JavaScript/find-largest-number.js
@@ -0,0 +1,7 @@
+const a = prompt("Enter First Number: ");
+const b = prompt("Enter Second Number: ");
+const c = prompt("Enter Third Number: ");
+
+const largest = Math.max(a, b, c);
+
+console.log("Largest Number is: " + largest);
